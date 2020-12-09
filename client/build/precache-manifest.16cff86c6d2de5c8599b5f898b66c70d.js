ac2ca35e3ec4d5201f38b7433af74425348041c1
psalm987 <samrythmn@gmail.com>
2020-12-09 15:55:38 +0100

    correction to make resources show

---
+++ client/build/precache-manifest.16cff86c6d2de5c8599b5f898b66c70d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54531dfd38955a620008b048966a83cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe43cfc153dac58226c6",
+    "url": "/static/css/main.2df4b933.chunk.css"
+  },
+  {
+    "revision": "2e41133a78768b32f882",
+    "url": "/static/js/2.f88a2316.chunk.js"
+  },
+  {
+    "revision": "cb5c9bef82b1b500bbeb6e747a258fde",
+    "url": "/static/js/2.f88a2316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe43cfc153dac58226c6",
+    "url": "/static/js/main.5b0b1910.chunk.js"
+  },
+  {
+    "revision": "372654281d7505ac7e36",
+    "url": "/static/js/runtime-main.1d338f2f.js"
+  },
+  {
+    "revision": "a3800ba0ad7c308308fffc38c2d8fd4c",
+    "url": "/static/media/Add User.a3800ba0.gif"
+  },
+  {
+    "revision": "b002158a4b48346fd508396950fb8b6b",
+    "url": "/static/media/Charts.b002158a.gif"
+  },
+  {
+    "revision": "a4ce3f07d8f795f08c8658c48e7c6bea",
+    "url": "/static/media/Customer Survey-amico.a4ce3f07.png"
+  },
+  {
+    "revision": "ef8b85a96cda9308e764aba19c0ab723",
+    "url": "/static/media/File bundle.ef8b85a9.gif"
+  },
+  {
+    "revision": "0490a8c0efb0333e0592e154bf218a2d",
+    "url": "/static/media/Library.0490a8c0.gif"
+  },
+  {
+    "revision": "886ab861f18a61a862989559ad1d5dba",
+    "url": "/static/media/News.886ab861.gif"
+  },
+  {
+    "revision": "c5f9c16a8a2cc6eb7ece6f6c734bfc14",
+    "url": "/static/media/file_search.c5f9c16a.svg"
+  },
+  {
+    "revision": "4f04c2c259c9c14135694796e5bc8f3d",
+    "url": "/static/media/update_image.4f04c2c2.svg"
+  }
+]);
